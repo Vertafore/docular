cbb8ff2b9116f230dc8ed6c8da304dee5a067566
John Martin <johndavidfive@gmail.com>
2013-04-13 10:38:22 -0700

    Adjusted the code to use the path.relative method instead of my crappy attempt. This helps especially in a dev environment for docular when npm link is used with sym links etc.. I also set some defaults in the options for docular to make things more legit as far as an options API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var exec = require('child_process').exec;
 var gen_docs = require(__dirname + '/lib/scripts/gen-docs.js');
+var path = require('path');
 
 module.exports = {
 
@@ -12,17 +13,8 @@ module.exports = {
 
     server: function(port, callBack) {
 
-        var relativeTOCWD = function (path) {
-            var relativePath = path.split(process.cwd())[1];
-            if(relativePath.substr(0,1) === "/" || relativePath.substr(0,1) === "\\") {
-                return relativePath.substr(1);
-            } else {
-                return relativePath;
-            }
-        };
-
         //Run the gen-docs script
-        var serverScript = relativeTOCWD(__dirname + '/lib/nodeserver/server.js ');
+        var serverScript = path.relative(process.cwd(), __dirname + '/lib/nodeserver/server.js ');
         var command_gen_docs = 'node ' + serverScript + port;
 
         exec(command_gen_docs, function(err, stdout, stderr) {
